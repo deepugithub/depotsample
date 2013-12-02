c219ce2743cb9b6d3ecb8ce16061a7a3e524e1fa
NCI <nci@nci-VirtualBox.(none)>
2013-12-02 04:33:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5ca0a9477058f0695890761dba604cb2d821496c2a6fb5972c99b48bb595a0a396e72dd9c8c6355db8c7dd0312affa4516d5b39e9f2c2d791da1da37850cbfb5'
